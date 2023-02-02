31313c5a05fe836a2381bde585de713b711545a2
Igor13-77 <navrocki77@gmail.com>
2023-02-02 11:35:28 +0200

    first commit

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трёхзначное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while ((n<100) || (n>=1000))
+{
+    Console.Write("Вы ошиблись!\nВведите трёхзначное число n: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int result = (n / 10) % 10;
+Console.WriteLine($"{n} -> {result} ");
